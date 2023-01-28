7eaafe6109523ef195ca5e0ec0bede19ea5237df
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-01-27 19:25:07 -0500

    Added basic select dropdown
    Co-authored-by: Parimal Kashireddy <pkashi1@users.noreply.github.com>
    Co-authored-by: May <lzhanCarMod@users.noreply.github.com>

--- src/components/DriversSection/components/RiderCard/RiderCard.tsx
+++ src/components/DriversSection/components/RiderCard/RiderCard.tsx
@@ -1,22 +1,36 @@
-import {Rider} from "../../../../types/all_types";
-import './RiderCard.css';
+import { Rider } from "../../../../types/all_types";
+import "./RiderCard.css";
 
 export const RiderCard = (props: { rider: Rider }) => {
-  const {rider} = props;
+  const { rider } = props;
 
-  return <div className={'rider-card'} data-status={rider.status}>
-    <div className={'row-1'}>
-      <div className={'left'}>
-        <p className={'name'}>{`${rider.name} (${rider.lsuEmail})`}</p>
-        <p className={'rider-request-placed-time'}>{new Date(rider.requestPlacedTime).toLocaleTimeString()}</p>
+  return (
+    <div className={"rider-card"} data-status={rider.status}>
+      <div className={"row-1"}>
+        <div className={"left"}>
+          <p className={"name"}>{`${rider.name} (${rider.lsuEmail})`}</p>
+          <p className={"rider-request-placed-time"}>
+            {new Date(rider.requestPlacedTime).toLocaleTimeString()}
+          </p>
+        </div>
+        <div className={"right"}>
+          <p className={"status"}>{`${rider.status}`}</p>
+        </div>
       </div>
-      <div className={'right'}>
-        <p className={'status'} >{`${rider.status}`}</p>
+      <div className={"row-2"}>
+        <div className={"left"}>
+          <p className={"pickup-location"}>{`From: ${rider.pickupLocation}`}</p>
+          <p className={"dropoff-location"}>{`To: ${rider.dropoffLocation}`}</p>
+        </div>
+        <div className={"right"}>
+          <select className={"status-select"}>
+            <option value={"pending"}>Pending</option>
+            <option value={"enroute"}>Enroute</option>
+            <option value={"pickedup"}>Picked Up</option>
+            <option value={"droppedoff"}>Dropped Off</option>
+          </select>
+        </div>
       </div>
     </div>
-    <div className={'row-2'}>
-      <p className={'pickup-location'}>{`From: ${rider.pickupLocation}`}</p>
-      <p className={'dropoff-location'}>{`To: ${rider.dropoffLocation}`}</p>
-    </div>
-  </div>
-}
+  );
+};
