eb1a711731dc5e93264b932e47b44612d5fbaa57
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-01-27 20:32:14 -0500

    Add rider to db functionality
    Co-authored-by: Parimal Kashireddy <pkashi1@users.noreply.github.com>
    Co-authored-by: May <lzhanCarMod@users.noreply.github.com>

--- src/components/RiderSection/RiderSection.tsx
+++ src/components/RiderSection/RiderSection.tsx
@@ -1,35 +1,113 @@
-import {PageSection} from "../PageSection/PageSection";
-import React, {useId} from "react";
-import './RiderSection.css';
+import { PageSection } from "../PageSection/PageSection";
+import React, { useId } from "react";
+import "./RiderSection.css";
+import { doc, setDoc } from "firebase/firestore";
+import { firebaseDb } from "../../backend/firebase";
+
+const LabeledInput = (props: {
+  formId: string;
+  name: string;
+  label: string;
+  placeholder?: string;
+  value?: string;
+  autoComplete?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  // onInput?: React.FormEventHandler<HTMLInputElement>;
+}) => {
+  const { formId, name, label, placeholder, value, autoComplete, onChange } =
+    props;
+  const inputId = `${formId}-${name}`;
+
+  return (
+    <div className={"input-container"}>
+      <label htmlFor={inputId}>{label}</label>
+      <input
+        id={inputId}
+        name={name}
+        autoComplete={autoComplete}
+        type={"text"}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
 
 export const RiderSection = () => {
   const formId = useId();
+
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [pickup, setPickup] = React.useState("");
+  const [dropdoff, setDropdoff] = React.useState("");
+
+  const requestRide = () => {
+    // console.log("CLICKED", e.target.value);
+
+    const newObject = {
+      name,
+      email,
+      pickup,
+      dropdoff,
+      status: "pending",
+    };
+
+    setDoc(doc(firebaseDb, "riders", email), newObject);
+
+    // const ref = doc(firebaseDb, "riders", rider.lsuEmail);
+    // updateDoc(ref, newObject);
+  };
 
   const requestRideForm = () => {
     const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
       e.preventDefault();
     };
 
-    const LabeledInput = (props: {
-      name: string, label: string, placeholder?: string, value?: string, autoComplete?: string,
-    }) => {
-      const {name, label, placeholder, value, autoComplete} = props;
-      const inputId = `${formId}-${name}`;
-
-      return <div className={'input-container'}>
-        <label htmlFor={inputId}>{label}</label>
-        <input id={inputId} name={name} autoComplete={autoComplete} type={'text'} placeholder={placeholder} value={value}/>
-      </div>
-    }
-
-    return <form className={'request-ride-form'} onSubmit={onSubmit}>
-      <LabeledInput name={'name'} label={'Name'} autoComplete={'name'}/>
-      <LabeledInput name={'lsuEmail'} label={'LSU Email'} autoComplete={'email'}/>
-      <LabeledInput name={'from'} label={'Pickup Location'} autoComplete={'street-address'}/>
-      <LabeledInput name={'to'} label={'Dropoff Location'} autoComplete={'street-address'}/>
-      <input type={"submit"} value={'Request Ride'}/>
-    </form>;
+    return (
+      <form className={"request-ride-form"} onSubmit={onSubmit}>
+        <LabeledInput
+          formId={formId}
+          name={"name"}
+          label={"Name"}
+          autoComplete={"name"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <LabeledInput
+          formId={formId}
+          name={"lsuEmail"}
+          label={"LSU Email"}
+          autoComplete={"email"}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <LabeledInput
+          formId={formId}
+          name={"from"}
+          label={"Pickup Location"}
+          autoComplete={"street-address"}
+          value={pickup}
+          onChange={(e) => setPickup(e.target.value)}
+        />
+        <LabeledInput
+          formId={formId}
+          name={"to"}
+          label={"Dropoff Location"}
+          autoComplete={"street-address"}
+          value={dropdoff}
+          onChange={(e) => setDropdoff(e.target.value)}
+        />
+        <input type={"submit"} value={"Request Ride"} onClick={requestRide} />
+      </form>
+    );
   };
 
-  return <PageSection className={'RiderSection'} sectionHeader={'Request a ride'} children={requestRideForm()}/>
-}
+  return (
+    <PageSection
+      className={"RiderSection"}
+      sectionHeader={"Request a ride"}
+      children={requestRideForm()}
+    />
+  );
+};
