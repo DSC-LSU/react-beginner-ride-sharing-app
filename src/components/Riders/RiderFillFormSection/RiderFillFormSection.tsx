afe76f6be353f539dba2b5d8a6b63e601f1d6f29
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-02-02 17:29:06 -0500

    Added TODOs and solution

--- src/components/Riders/RiderFillFormSection/RiderFillFormSection.tsx
+++ src/components/Riders/RiderFillFormSection/RiderFillFormSection.tsx
@@ -27,7 +27,33 @@ const LabeledInput = (props: {
   } = props;
   const inputId = `${formId}-${name}`;
 
-  return <div className={"input-container"}></div>;
+  return (
+    <div className={"input-container"}>
+      {/* TODO 4: Add <label htmlFor={inputId}>{label}</label> */}
+      {/* TODO 5: Add <input
+        id={inputId}
+        name={name}
+        autoComplete={autoComplete}
+        type={"text"}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      /> */}
+
+      <label htmlFor={inputId}>{label}</label>
+      <input
+        id={inputId}
+        name={name}
+        autoComplete={autoComplete}
+        type={"text"}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      />
+    </div>
+  );
 };
 
 type RiderFillFormSectionProps = {
@@ -38,6 +64,11 @@ type RiderFillFormSectionProps = {
 export const RiderFillFormSection = (props: RiderFillFormSectionProps) => {
   const formId = useId();
 
+  // TODO 1: Add state for name, pickup, and dropoff
+  const [name, setName] = React.useState("");
+  const [pickup, setPickup] = React.useState("");
+  const [dropdoff, setDropdoff] = React.useState("");
+
   const requestRide = () => {};
 
   const requestRideForm = () => {
@@ -45,7 +76,45 @@ export const RiderFillFormSection = (props: RiderFillFormSectionProps) => {
       e.preventDefault();
     };
 
-    return <form className={"request-ride-form"} onSubmit={onSubmit}></form>;
+    return (
+      <form className={"request-ride-form"} onSubmit={onSubmit}>
+        {/* TODO 2: Call <LabeledInput /> for name, email, pickup, and dropoff */}
+        {/* TODO 3: <input type="submit"/> */}
+        <LabeledInput
+          formId={formId}
+          name={"name"}
+          label={"Name"}
+          autoComplete={"name"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <LabeledInput
+          formId={formId}
+          name={"email"}
+          label={"Email"}
+          autoComplete={"email"}
+          value={props.user?.email ?? ""}
+          disabled={true}
+        />
+        <LabeledInput
+          formId={formId}
+          name={"from"}
+          label={"Pickup Location"}
+          autoComplete={"street-address"}
+          value={pickup}
+          onChange={(e) => setPickup(e.target.value)}
+        />
+        <LabeledInput
+          formId={formId}
+          name={"to"}
+          label={"Dropoff Location"}
+          autoComplete={"street-address"}
+          value={dropdoff}
+          onChange={(e) => setDropdoff(e.target.value)}
+        />
+        <input type={"submit"} value={"Request Ride"} onClick={requestRide} />
+      </form>
+    );
   };
 
   return (
