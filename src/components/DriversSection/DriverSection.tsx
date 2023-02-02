05ae490e08ef1e83d00b53821082b6b14894b784
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-02-01 22:23:56 -0500

    Added solution and TODOs

--- src/components/DriversSection/DriverSection.tsx
+++ src/components/DriversSection/DriverSection.tsx
@@ -7,11 +7,40 @@ import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
 import { firebaseDb } from "../../backend/firebase";
 
 export const DriverSection = () => {
+  // TODO 1: Create a state variable riders and a setter setRiders
+  const [riders, setRiders] = React.useState<Array<Rider>>([]);
+
   React.useEffect(() => {
-    // Get riders from Firestore
+    // TODO 2: Create query()
+    // TODO 2.1: collection(firebaseDb, "riders")
+    // TODO 2.2: orderBy("requestPlacedTime", "desc")
+
+    const q = query(
+      collection(firebaseDb, "riders"),
+      orderBy("requestPlacedTime", "desc")
+    );
+
+    // TODO 3: Create onSnapshot()
+    // TODO 3.1: Create newRiders from querySnapshot.docs.map()
+    // TODO 3.2: setRiders(newRiders)
+
+    onSnapshot(q, (querySnapshot) => {
+      const newRiders: Array<Rider> = querySnapshot.docs.map<Rider>((doc) => ({
+        name: doc.data().name,
+        email: doc.id,
+        requestPlacedTime: doc.data().requestPlacedTime.seconds * 1000,
+        pickupLocation: doc.data().pickupLocation,
+        dropoffLocation: doc.data().dropoffLocation,
+        status: doc.data().status,
+      }));
+      setRiders(newRiders);
+    });
   }, []);
 
   const riderCards = () => <ul className={"riders"}></ul>;
+
+  // TODO 4: console.log(riders)
+  console.log(riders);
 
   return (
     <PageSection
